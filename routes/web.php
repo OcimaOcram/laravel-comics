cf0c0d8eeb4ab7a42b094b44b6c5385e50d82d07
OcimaOcram <marco.amico.pesci00@gmail.com>
2022-07-12 17:10:55 +0200

    Esercizio consegnato

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $comics = config('comic');
+    $comics = config('comics');
     return view('home', [
         'comics' => $comics,
 
